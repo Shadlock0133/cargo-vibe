7d2ec013c927d19cf8aa714ca59228c850bd1c47
Aurora <tomek-kubel2@wp.pl>
2022-10-28 19:59:12 +0200

    add check subcommand

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ enum Opt {
 #[derive(Subcommand)]
 enum Cmd {
     Build,
+    Check,
 }
 
 async fn start_client() -> ButtplugClient {
@@ -28,6 +29,17 @@ async fn start_client() -> ButtplugClient {
     sleep(Duration::from_secs(1)).await;
     client.stop_scanning();
     client
+}
+
+async fn vibrate_all(client: &ButtplugClient, speed: f64, duration: Duration) {
+    for device in client.devices() {
+        device
+            .vibrate(&buttplug::client::VibrateCommand::Speed(speed))
+            .await
+            .unwrap();
+    }
+    sleep(duration).await;
+    client.stop_all_devices().await.unwrap();
 }
 
 fn is_success(stdout: Vec<u8>) -> bool {
@@ -52,17 +64,16 @@ fn is_success(stdout: Vec<u8>) -> bool {
     false
 }
 
+const CARGO_JSON_FLAG: &str =
+    "--message-format=json-diagnostic-rendered-ansi,json-render-diagnostics";
+
 #[tokio::main]
 async fn main() {
+    let client = spawn(start_client());
     match Opt::parse() {
         Opt::Vibe(Cmd::Build) => {
-            let client = spawn(start_client());
             let cmd = Command::new("cargo")
-                .args(&[
-                    "build",
-                    "--message-format=json-diagnostic-rendered-ansi,\
-                        json-render-diagnostics",
-                ])
+                .args(&["build", CARGO_JSON_FLAG])
                 .stdout(Stdio::piped())
                 .spawn()
                 .unwrap();
@@ -71,16 +82,25 @@ async fn main() {
             if is_success(output.stdout) {
                 eprintln!("[cargo-vibe] build successful!");
                 let client = client.await.unwrap();
-                for device in client.devices() {
-                    device
-                        .vibrate(&buttplug::client::VibrateCommand::Speed(1.0))
-                        .await
-                        .unwrap();
-                }
-                sleep(Duration::from_secs(3)).await;
-                client.stop_all_devices().await.unwrap();
+                vibrate_all(&client, 1.0, Duration::from_secs(3)).await;
             } else {
                 eprintln!("[cargo-vibe] build failed");
+            }
+        }
+        Opt::Vibe(Cmd::Check) => {
+            let cmd = Command::new("cargo")
+                .args(&["check", CARGO_JSON_FLAG])
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap();
+
+            let output = cmd.wait_with_output().unwrap();
+            if is_success(output.stdout) {
+                eprintln!("[cargo-vibe] check successful!");
+                let client = client.await.unwrap();
+                vibrate_all(&client, 1.0, Duration::from_secs(3)).await;
+            } else {
+                eprintln!("[cargo-vibe] check failed");
             }
         }
     }
